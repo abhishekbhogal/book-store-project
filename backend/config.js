e2eb16aa4e05809ad681b0ea0023d01f4ab6c85c
Abhishek Bhogal <abhishek.bhogal7@gmail.com>
2023-09-04 11:12:30 -0500

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 500;
 
 // MONGO DB PASSWORD: MWYLvChS2KhJevHT
-export const mongoDBURL = 'mongodb+srv://bhogalabhishek03:MWYLvChS2KhJevHT@book-store-mern.tpxkyot.mongodb.net/?retryWrites=true&w=majority'
+export const mongoDBURL = ${{ secrets.MY_GITHUB_TOKEN }}
